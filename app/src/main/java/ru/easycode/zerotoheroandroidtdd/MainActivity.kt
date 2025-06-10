60c2e9f82007c5ec0b547fec4770b807c1719e48
857l <nik.gusev.2017@bk.ru>
2025-06-10 19:53:15 +0300

    task11 part2 solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,13 +1,16 @@
 package ru.easycode.zerotoheroandroidtdd
 
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var textView: TextView
+    private val count: Count = Count.Base(step = 2)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,11 +21,22 @@ class MainActivity : AppCompatActivity() {
 
         button.setOnClickListener {
             val value = textView.text.toString()
-            val count: Count = Count.Base(step = 2)
-            textView.text = count.increment(textView.text.toString())
+            textView.text = count.increment(value)
         }
 
     }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(KEYTEXT, textView.text.toString())
+    }
 
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        textView.text = savedInstanceState.getString(KEYTEXT)
+    }
+
+    companion object {
+        private const val KEYTEXT = "textValue"
+    }
 }
