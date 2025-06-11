1a0ab73340131d80ce6dde41171e3265c32d138e
857l <nik.gusev.2017@bk.ru>
2025-06-11 14:06:43 +0300

    task12 part2 solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -7,25 +7,88 @@ import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var textView : TextView
+    private val count = Count.Base(2, 4)
+    private lateinit var textView: TextView
+    private lateinit var button: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         textView = findViewById<TextView>(R.id.countTextView)
-        val button = findViewById<Button>(R.id.incrementButton)
+        button = findViewById<Button>(R.id.incrementButton)
 
-        button.setOnClickListener{
-
+        button.setOnClickListener {
+            val uiState: UiState = count.increment(textView.text.toString())
+            uiState.apply(textView, button)
         }
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
+        outState.putString(KEYTEXT, textView.text.toString())
+        outState.putBoolean(KEYBUTTON, button.isEnabled)
     }
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
+        textView.text = savedInstanceState.getString(KEYTEXT)
+        button.isEnabled = savedInstanceState.getBoolean(KEYBUTTON)
     }
+
+    companion object {
+        private const val KEYTEXT = "textValue"
+        private const val KEYBUTTON = "buttonEnable"
+    }
+}
+
+
+interface Count {
+    fun increment(number: String): UiState
+
+    class Base(private val step: Int, private val max: Int) : Count {
+
+        init {
+            if (step <= 0)
+                throw IllegalStateException("step should be positive, but was $step")
+
+            if (max <= 0)
+                throw IllegalStateException("max should be positive, but was $max")
+
+            if (max < step)
+                throw IllegalStateException("max should be more than step")
+        }
+
+        override fun increment(number: String): UiState {
+            val value = number.toInt()
+            val result = value + step
+            return if (result + step <= max)
+                UiState.Base(result.toString())
+            else
+                UiState.Max(result.toString())
+        }
+
+    }
+
+}
+
+interface UiState {
+
+    fun apply(textView: TextView, button: Button)
+
+    data class Base(private val text: String) : UiState {
+        override fun apply(textView: TextView, button: Button) {
+            textView.text = text
+        }
+
+    }
+
+    data class Max(private val text: String) : UiState {
+        override fun apply(textView: TextView, button: Button) {
+            textView.text = text
+            button.isEnabled = false
+        }
+
+    }
+
 }
