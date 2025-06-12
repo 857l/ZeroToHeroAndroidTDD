7ad8de0d3bc025103d7cb29db184f0a01509e2f6
857l <nik.gusev.2017@bk.ru>
2025-06-12 23:06:27 +0300

    task15 part 1

---
+++ app/src/test/java/ru/easycode/zerotoheroandroidtdd/LiveDataWrapper.kt
@@ -0,0 +1,9 @@
+package ru.easycode.zerotoheroandroidtdd
+
+import androidx.lifecycle.LiveData
+
+interface LiveDataWrapper {
+
+    fun update(value: UiState)
+    fun liveData(): LiveData<UiState>
+}
---
+++ app/src/test/java/ru/easycode/zerotoheroandroidtdd/MainViewModel.kt
@@ -0,0 +1,8 @@
+package ru.easycode.zerotoheroandroidtdd
+
+class MainViewModel(private val liveDataWrapper: LiveDataWrapper, private val repository: Repository) {
+    fun load() {
+        TODO("Not yet implemented")
+    }
+
+}
