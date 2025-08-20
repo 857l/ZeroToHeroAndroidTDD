d3790b3995e194c053791715d803a5d7a4eb21d1
857l <nik.gusev.2017@bk.ru>
2025-08-20 16:42:08 +0300

    commit

--- app/src/test/java/ru/easycode/zerotoheroandroidtdd/list/ListScreen.kt
+++ app/src/test/java/ru/easycode/zerotoheroandroidtdd/list/ListScreen.kt
@@ -1,10 +1,5 @@
 package ru.easycode.zerotoheroandroidtdd.list
 
-import androidx.fragment.app.FragmentManager
 import ru.easycode.zerotoheroandroidtdd.main.Screen
 
-object ListScreen : Screen {
-    override fun show(supportFragmentManager: FragmentManager, containerId: Int) {
-        TODO("Not yet implemented")
-    }
-}
+object ListScreen : Screen.Replace(ListFragment::class.java)
