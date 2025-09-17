2152b16484e71a93543ea9cb866327cba049bb32
857l <nik.gusev.2017@bk.ru>
2025-09-17 20:20:52 +0300

    task 019 part 1 solved

---
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/Repository.kt
@@ -0,0 +1,25 @@
+package ru.easycode.zerotoheroandroidtdd
+
+import java.net.UnknownHostException
+
+interface Repository {
+
+    suspend fun load(): LoadResult
+
+    class Base(
+        private val service: SimpleService,
+        private val url: String
+    ) : Repository {
+
+        override suspend fun load(): LoadResult {
+            return try {
+                val result = service.fetch(url)
+                LoadResult.Success(SimpleResponse(result.text))
+            } catch (e: UnknownHostException) {
+                LoadResult.Error(true)
+            } catch (e: IllegalStateException) {
+                LoadResult.Error(false)
+            }
+        }
+    }
+}
