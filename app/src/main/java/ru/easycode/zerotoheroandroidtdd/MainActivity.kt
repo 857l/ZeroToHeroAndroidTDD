9b68a7dc9f46a0626e2f7fb681c768aac98e0abc
857l <nik.gusev.2017@bk.ru>
2025-11-26 20:35:22 +0300

    task 034 solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -3,11 +3,19 @@ package ru.easycode.zerotoheroandroidtdd
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import ru.easycode.zerotoheroandroidtdd.ui.theme.ZeroToHeroAndroidTDDTheme
+import java.io.Serializable
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,9 +26,33 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    //todo
+                    Column {
+                        var value: Count by rememberSaveable {
+                            mutableStateOf(Count.Base(0, 1))
+                        }
+                        Text(text = value.toString())
+                        Button(onClick = {
+                            value = value.increment()
+                        }) {
+                            Text(text = "increment")
+                        }
+                    }
                 }
             }
         }
+    }
+}
+
+interface Count : Serializable {
+
+    fun increment(): Count
+
+    data class Base(
+        private val value: Int,
+        private val step: Int
+    ) : Count {
+        override fun increment(): Count = Base(value + step, step)
+        override fun toString(): String = value.toString()
+
     }
 }
